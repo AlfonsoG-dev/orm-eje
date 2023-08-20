5c57812dde5febcd60b0d3af1cbadebc84b4de5f
tuNovio <alfgomachi@gmail.com>
2023-08-20 09:43:41 -0500

    refactor: se crean las operaciones para hacer con sultas personalizadas

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
 const operation = require('./services/Operations')
 
-console.log(operation.test_db('consulta'))
+operation.test_db('consulta')
 operation.create_table('consulta')
--- src/services/Operations.js
+++ src/services/Operations.js
@@ -2,27 +2,44 @@
 const db_conection = require('../services/DbConection')
 const User = require('../model/DbModel')
 const utils = require('../utils/DbUtils')
+const {AxiosHeaders} = require('axios')
 
 //utilidades
-const con = db_conection.normal_conection("localhost", "test_user", "5x5W12")
+const cursor = db_conection.normal_conection("localhost", "test_user", "5x5W12")
 const trim = utils.get_clean_properties(User)
 
 //operaciones
 const operations = {
     test_db: function (db_name = ''){
-        const data = con.connect()
+        const data = cursor.connect()
         if(data !== undefined){
             return data
         }else{
-            const data = con.execute(`create database if not exists ${db_name}`)
+            const data = cursor.execute(`create database if not exists ${db_name}`)
             return data
         }
     },
     create_table: async function(db_name = ''){
-        const data = await con.execute(`create table if not exists ${db_name}.test(${trim})`)
+        const data = await cursor.execute(`create table if not exists ${db_name}.test(${trim})`)
         if(data !== undefined){
             console.log(data)
         }
+    },
+    any_query: function(sql = '', ...args){
+        return new Promise((resolve, reject) => {
+            cursor.query(sql, args, function(err, res) {
+                if(err) reject(err)
+                resolve(res)
+            })
+        })
+    },
+    any_execute: function(sql = '', ...args){
+        return new Promise((resolve, reject) => {
+            cursor.execute(sql, args, function(err, res) {
+                if(err) reject(err)
+                resolve(rs)
+            })
+        })
     }
 }
 
