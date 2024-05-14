6e417e6fca4c1f912af931ed828ea12cc1a76c3e
AlfonsoG-dev <alfgomachi@gmail.com>
2024-05-13 23:22:36 -0500

    refactor: use env data to create connection

--- src/App.js
+++ src/App.js
@@ -9,13 +9,12 @@ const model = new User()
 model.initDB()
 
 // instance of database connection
-const cursor = conn.normal_conection()
+const cursor = conn.normal_conection('consulta')
 
 // database and table operations
 const op = new Operaciones('consulta', 'users', cursor, model)
-
 /*
 op.make_migrations()
-.then((res) => {return res})
-.catch((err) => {throw Error(err)})
+    .then((res) => {console.log(res)})
+    .catch((err) => {throw Error(err)})
 */
--- src/services/DbConection.js
+++ src/services/DbConection.js
@@ -1,21 +1,14 @@
 const mysql = require('mysql2')
+const DbConfig = require('../utils/DbConfig')
+
+const config = new DbConfig();
+
 const db_conection = {
     normal_conection: function(db_name = '') {
-        return mysql.createConnection({
-            host: 'localhost',
-            user: 'test_user',
-            password: '5x5W12',
-            database: db_name
-        })
+        return mysql.createConnection(config.normal_config(db_name))
     },
     pool_conection: function(db_name = '') {
-        return mysql.createPool({
-            connectionLimit: 10,
-            host: 'localhost',
-            user: 'test_user',
-            password: '5x5W12',
-            database: db_name
-        })
+        return mysql.createPool(config.pool_config(db_name))
     }
 }
 
